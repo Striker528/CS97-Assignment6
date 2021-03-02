30905297e08c187971f21e18df1c09078916784a
Austyn Joseph Adams <classaad@lnxsrv10.seas.ucla.edu>
2021-03-01 16:41:07 -0800

    Finished the first part, no on #2

--- topo_order_commits.py
+++ topo_order_commits.py
@@ -15,30 +15,42 @@ class CommitNode:
 
 
 def get_git_directory():
-    #stuff
     current_directory = os.getcwd()
-    no_top_git = "Did not find top .git"
-    while ".git" != current_directory:
-        if( os.path.exists() == True):
-            current_directory = os.path.dirname()
+    while ('/' != current_directory):
+        if( os.path.exists(current_directory + "/.git")):
+            return current_directory
         else:
-            return no_top_git
-    return current_directory
+            if (os.path.exists() == False):
+                exit(1)
+            else:
+                current_directory = os.path.dirname(current_directory)
+    print("Not inside a git respository")
+    exit(1)
 
 
-
-
-def get_list_local_branches():
-    #stuff
-    
-
-
-
-
+def get_list_local_branches(git_dir):
+    #Normal loop of getting all the directores in one list and looping through that. 
+    #Get all the folders in the .git repo
+    contents_dir = os.listdir(git_dir)
+    local_branch_heads = []
+    #Then loop through all the folders
+    for content in contents_dir:
+        #if there is a file
+        if (os.path.isfile(conent)):
+            #In each folder, open up the file
+            file = open(content, 'r')
+            #read the first line from the file and append that to the branch heads
+            local_branch_heads.append[content, file.readline().strip()]
+        #else, if the item is a directory
+        else:
+            #add the directory to the directory list
+            contents_dir.append(content)
+    return local_branch_heads
 
 
 def build_commit_graph(git_dir, local_branch_heads):
-    commit_nodes = {} #Represents your graph
+    #Represents your graph
+    commit_nodes = {} 
     visited = set() 
     stack = local_branch_heads
     while stack:
@@ -64,8 +76,7 @@ def build_commit_graph(git_dir, local_branch_heads):
             if p not in visited:
                 #Replace with Code - What do we do if p isn’t in visited?
             
-            
-            
+
             if p not in commit_nodes:
                 #Replace with Code - What do we do if p isn’t in commit_nodes (graph)?    
             
@@ -77,13 +88,21 @@ def build_commit_graph(git_dir, local_branch_heads):
 
 
 def topological_sort(commit_nodes):
-    result = []           # commits we have processed and are now sorted
-    no_children = deque() # commits we can process now 
-    copy_graph = copy.deepcopy(commit_nodes) # Copy graph so we don't erase info
+    # commits we have processed and are now sorted
+    result = []
+
+    # commits we can process now
+    no_children = deque()
+
+    # Copy graph so we don't erase info
+    copy_graph = copy.deepcopy(commit_nodes)
+
+
     # If the commit has no children, we can process it
     for commit_hash in copy_graph:
         if len(copy_graph[commit_hash].children) == 0:
             no_children.append(commit)
+
     #Loop through until all commits are processed
     while len(no_children) > 0:
         commit_hash = no_children.popleft()
@@ -123,22 +142,21 @@ def print_topo_ordered_commits_with_branch_names(commit_nodes, topo_ordered_co
             print(f'{sticky_hash}=\n')
 
 
-
 def topo_order_commits():
     #Get git directory (can be helper function)
-    get_git_directory()
+    get_dir = get_git_directory()
 
     #Get list of local branch names (can be helper function)
-    get_list_local_branches()
+    local_branch_heads = get_list_local_branches(get_dir)
 
     #Build the commit graph (can be helper function)
-    build_commit_graph()
+    commit_nodes = build_commit_graph(git_dir, local_branch_heads)
 
     #Topologically sort the commit graph (can be helper fnction)
-    topological_sort()
+    topo_ordered_commits = topological_sort(commit_nodes)
 
     #Print the sorted order (can be helper function)
-    print_topo_ordered_commits_with_branch_names()
+    print_topo_ordered_commits_with_branch_names(commit_nodes, topo_ordered_commits, head_to_branches)
 
 
 if __name__ == '__main__':
